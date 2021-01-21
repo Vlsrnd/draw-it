3366272decf597751ed9d3ffe85fd0c28a99f52f
Leonid Velichko <vlsrnd@gmail.com>
2021-01-21 22:22:00 +0300

    feat(index.js): add getColorMap function. this add img to canvas, pixelize this and create imageDate pixelized img

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const root = document.querySelector('#root');
 
-getAverageColor = (imageData) => {
+const getAverageColor = (imageData) => {
+  const average = (arr) => arr.reduce((acc, elem) => acc + elem) / arr.length;
   const r = [];
   const g = [];
   const b = [];
@@ -19,10 +20,62 @@ getAverageColor = (imageData) => {
 const canvas = document.createElement('canvas');
 root.append(canvas);
 
-// const getColorMap = async (image, config, canvas) => {
-//   const {imgW, imgH, imgURL} = config;
-//   canvas.setAttribute('width', imgW);
-//   canvas.setAttribute('height', imgH);
+const getColorMap = (config) => {
+  const img = new Image();
+  img.src = config.imgURL;
 
-//   const img = new Image();
-// };
+  const onLoadCreator = (config) => (event) => {
+    const {
+      imgW,
+      imgH,
+      pixelSize,
+      drawAnimationMode,
+      canvas
+    } = config;
+    const ctx = canvas.getContext('2d');
+
+    canvas.setAttribute('width', imgW);
+    canvas.setAttribute('height', imgH);
+
+    ctx.drawImage(event.currentTarget, 0, 0, imgW, imgH);
+    for (let y = 0; y < imgH / pixelSize; y++) {
+      for (let x = 0; x < imgW / pixelSize; x++) {
+        const xyWH = [
+          x * pixelSize,
+          y * pixelSize,
+          Math.min(pixelSize, imgW - x * pixelSize),
+          Math.min(pixelSize, imgH - y * pixelSize)
+        ];
+        const imageData = ctx.getImageData(...xyWH);
+        ctx.fillStyle = getAverageColor(imageData);
+        ctx.fillRect(...xyWH);
+      }
+    }
+
+    const colorMaskImageData = ctx.getImageData(0, 0, imgW, imgH);
+    const colorMask = {};
+    drawAnimationMode && ctx.clearRect(0, 0, imgW, imgH);
+    for (let y = 0; y < imgH; y++) {
+      for (let x = 0; x < imgW; x++) {
+        const r = colorMaskImageData.data[((x + (y * imgW)) * 4) + 0];
+        const g = colorMaskImageData.data[((x + (y * imgW)) * 4) + 1];
+        const b = colorMaskImageData.data[((x + (y * imgW)) * 4) + 2];
+        const a = colorMaskImageData.data[((x + (y * imgW)) * 4) + 3];
+        colorMask[`x${x}y${y}`] = `rgba(${r}, ${g}, ${b}, ${a})`;
+      }
+    }
+  };
+
+  img.onload = onLoadCreator(config);
+
+};
+
+const config = {
+  imgW: 480,
+  imgH: 270,
+  imgURL: './img/elbrus.jpg',
+  pixelSize: 10,
+  drawAnimationMode: false,
+  canvas: canvas,
+}
+getColorMap(config, canvas);
